57b1f7668032d3b70fb24e8dda8739016ba66299
Tyler Collins <69768223+tcollins6049@users.noreply.github.com>
2023-08-01 18:58:30 -0500

    Add files via upload

---
+++ src/main/client/Demo.java
@@ -0,0 +1,20 @@
+package client;
+
+/**
+ * Demo used to manually test the program.
+ * 
+ * @author 
+ * @version 
+ */
+public class Demo
+{
+	/**
+     * Runs the demo.
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String[] args) 
+    {
+
+    }
+}
